8a5c63d41e3ed31743ce209b9c76ab6fd8121e03
Carlos Campos <csmcprojects.geral@gmail.com>
2015-08-29 22:18:07 +0100

    Corrections
    csmc.php - Moved path defines to index.php for ease of configuration in
    future updates so that the entire csmc folder can be replaced without
    needing to update the paths that depend from server to server.
    
    index.php - Added path defines as explained.

--- csmc/native/csmc.php
+++ csmc/native/csmc.php
@@ -17,14 +17,6 @@ use csmc\native\oauth\session as session;
 class csmc{
 
 	public function __construct(){
-		//Defines global variables used by the framework
-		define("CSMC_CORE_ROOT", $_SERVER["DOCUMENT_ROOT"]."/csmc");
-		define("CSMC_NATIVE_ROOT", CSMC_CORE_ROOT."/native/");
-		define("CSMC_MODULES_ROOT", CSMC_CORE_ROOT."/modules/".__INSTANCE__."/");
-		define("INSTANCE_CONFIG_ROOT", CSMC_CORE_ROOT."/instance.".__INSTANCE__.".config");
-		define("AUTOLOAD_ROOT", CSMC_CORE_ROOT."/autoload.php");
-		define("NATIVE_NAMESPACE", 'csmc\\native\\');
-		define("MODULE_NAMESPACE", 'csmc\\modules\\'.__INSTANCE__.'\\');
 		//Autoload mechanism, follows PSR-0 specifications
 		//Autoload for native classes and modules classes
 		require_once(AUTOLOAD_ROOT);
--- csmc_front/index.php
+++ csmc_front/index.php
@@ -1,4 +1,13 @@
 <?php
+	//Defines global variables used by the framework
+	define("CSMC_CORE_ROOT", $_SERVER["DOCUMENT_ROOT"]."/csmc");
+	define("CSMC_NATIVE_ROOT", CSMC_CORE_ROOT."/native/");
+	define("CSMC_MODULES_ROOT", CSMC_CORE_ROOT."/modules/".__INSTANCE__."/");
+	define("INSTANCE_CONFIG_ROOT", CSMC_CORE_ROOT."/instance.".__INSTANCE__.".config");
+	define("AUTOLOAD_ROOT", CSMC_CORE_ROOT."/autoload.php");
+	define("NATIVE_NAMESPACE", 'csmc\\native\\');
+	define("MODULE_NAMESPACE", 'csmc\\modules\\'.__INSTANCE__.'\\');
+
 	define("__INSTANCE__", "webapp"); //the name of the config file that will be called instance.thisinstancename.config
 	define("__DEV_ENVIRONMENT__", 1); //0 (production mode) and 1 (development mode)
 	//Requires the framework initiator file.
